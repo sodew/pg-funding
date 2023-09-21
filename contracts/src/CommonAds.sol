cbd1c1e3e339c6d5206f1f7373b560e513c353be
sophiahdew <59624772+sophiahdew@users.noreply.github.com>
2023-09-21 13:26:31 +0200

    WIP

---
+++ contracts/src/CommonAds.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC721} from "solady/tokens/ERC721.sol";
+import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+
+contract CommonAds is ERC721 {
+    using FixedPointMathLib for uint256;
+    using SafeTransferLib for address;
+
+    struct Metadata {
+        string name;
+        string desc;
+        string link;
+        string img;
+    }
+
+    struct Spot {
+        uint256 setPrice;
+        uint256 lastUpdatedAt;
+        bytes32 metaId;
+        bool inAuction;
+    }
+
+    struct SpotDetails {
+        Metadata metadata;
+        uint256 price;
+        bool inAuction;
+    }
+
+    struct Space {
+        address owner;
+        uint256 totalSpots;
+        bytes32 metaId;
+        mapping(uint256 => Spot) spots;
+    }
+
+    uint256 internal nextSpaceId;
+    int256 internal immutable AUCTION_DECAY;
+    mapping(bytes32 metaId => Metadata meta) public getMetadata;
+    mapping(uint256 spaceId => Space) public spaces;
+    mapping(address user => uint256) public balances;
+
+    error NotSpotOwner();
+    error PaymentBelowPrice();
+
+    constructor() {
+        // AUCTION_DECAY =
+    }
+
+    function setMetadata(uint256 subId, Metadata calldata meta) external {
+        bytes32 metaId = _getMetaId(subId);
+        getMetadata[metaId] = meta;
+    }
+
+    function create(uint256 subId, uint256[] calldata prices) external {
+        // TODO: Remove?
+        uint256 totalPrices = prices.length;
+        require(totalPrices == 3);
+        uint256 spaceId;
+        unchecked {
+            spaceId = ++nextSpaceId;
+        }
+        Space storage space = spaces[spaceId];
+        space.metaId = _getMetaId(subId);
+        space.owner = msg.sender;
+        space.totalSpots = totalPrices;
+        for (uint256 i = 0; i < totalPrices;) {
+            _mint(msg.sender, _getSpotId(spaceId, i));
+            space.spots[i] = Spot({
+                lastUpdatedAt: block.timestamp,
+                setPrice: prices[i],
+                metaId: bytes32(0), // TODO: Make metadata getter return default for empty
+                inAuction: true
+            });
+            // forgefmt: disable-next-item
+            unchecked { ++i; }
+        }
+    }
+
+    function setPrice(uint256 spotId, uint256 newPrice) external {
+        if (!_authorized(msg.sender, spotId)) revert NotSpotOwner();
+        _getSpot(spotId).setPrice = newPrice;
+    }
+
+    function buy(uint256 spotId, uint256 metaSubId, uint256 newPrice) external payable {
+        uint256 price = getPrice(spotId);
+        if (msg.value < price) revert PaymentBelowPrice();
+        Spot storage spot = _getSpot(spotId);
+        _transfer(ownerOf(spotId), msg.sender, spotId);
+        spot.setPrice = newPrice;
+        spot.lastUpdatedAt = block.timestamp;
+        spot.metaId = _getMetaId(metaSubId);
+        unchecked {
+            uint256 refund = msg.value - price;
+            if (refund > 0) msg.sender.safeTransferETH(refund);
+        }
+    }
+
+    function getSpace(uint256 spaceId) external view returns (address owner, SpotDetails[] memory spots) {
+        Space storage space = spaces[spaceId];
+        owner = space.owner;
+        uint256 totalSpots = space.totalSpots;
+        spots = new SpotDetails[](totalSpots);
+        for (uint256 i = 0; i < totalSpots;) {
+            Spot storage spot = space.spots[i];
+            spots[i].metadata = getMetadata[spot.metaId];
+            spots[i].price = getPrice(_getSpotId(spaceId, i));
+            spots[i].inAuction = spot.inAuction;
+            // forgefmt: disable-next-item
+            unchecked { ++i; }
+        }
+    }
+
+    function getPrice(uint256 spotId) public view returns (uint256) {
+        Spot storage spot = _getSpot(spotId);
+        return spot.setPrice;
+    }
+
+    function name() public pure override returns (string memory) {
+        return "Common Ads";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "CAD";
+    }
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        // TODO: Add URI
+        return "MISSING";
+    }
+
+    function _getSpotId(uint256 spaceId, uint256 spotIndex) internal pure returns (uint256 spotId) {
+        assert(spotIndex < 256);
+        spotId = (spaceId << 8) | spotIndex;
+    }
+
+    function _getMetaId(uint256 subId) internal view returns (bytes32 metaId) {
+        assembly {
+            mstore(0x00, subId)
+            mstore(0x20, caller())
+            metaId := keccak256(0x00, 0x40)
+        }
+    }
+
+    function _getSpot(uint256 spotId) internal view returns (Spot storage spot) {
+        uint256 spaceId = spotId >> 8;
+        uint256 spotIndex = spotId & 0xff;
+        return spaces[spaceId].spots[spotIndex];
+    }
+
+    function _authorized(address operator, uint256 tokenId) internal view returns (bool) {
+        return ownerOf(tokenId) == operator;
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { getContract } from "../ethereum";
+import Counter from "../contracts/out/Counter.sol/Counter.json";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const [contract, setContract] = useState(null);
+  const [projectId, setProjectId] = useState("");
+  const [projectName, setProjectName] = useState("");
+  const [projectBalance, setProjectBalance] = useState(0);
+  const [nfts, setNfts] = useState([]);
+
+  useEffect(() => {
+    async function initContract() {
+      const contract = getContract(
+        "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        Counter.abi,
+        0 // Use the first account as the signer
+      );
+      setContract(contract);
+    //   const initialCount = await contract.getCount();
+    //   setCount(initialCount.toNumber());
+    }
+    initContract();
+  }, []);
+
+  async function createProject(name) {
+    if (!contract) return;
+    // const tx = await contract.createProject(name);
+    // await tx.wait();
+    // Optionally, you can refresh the UI or navigate the user to the new project.
+
+    const tx = await contract.increment();
+    await tx.wait();
+    const updatedCount = await contract.getCount();
+    setCount(updatedCount.toNumber());
+  }
+
+  async function fetchProject(id) {
+    // if (!contract) return;
+    // const project = await contract.getProjectById(id);
+    // setProjectName(project.name);
+    // setProjectBalance(project.balance.toNumber());
+    // setNfts(project.nfts); // Assuming NFTs are stored as an array.
+    setProjectName(id);
+  }
+
+  async function increment() {
+    if (!contract) return;
+    const tx = await contract.increment();
+    await tx.wait();
+    const updatedCount = await contract.getCount();
+    setCount(updatedCount.toNumber());
+  }
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Open Source Project Auction</h1>
+
+      <h2>Projects created so far: {count}</h2>
+      
+      <h2>Create a New Project</h2>
+      <input
+        placeholder="Project Name"
+        onChange={(e) => setProjectName(e.target.value)}
+      />
+      <button onClick={() => createProject(projectName)}>Create Project</button>
+      
+      <h2>Fetch Project</h2>
+      <input
+        placeholder="Project ID"
+        onChange={(e) => setProjectId(e.target.value)}
+      />
+      <button onClick={() => fetchProject(projectId)}>Fetch Project</button>
+      
+      {projectName && (
+        <div>
+          <h3>Project: {projectName}</h3>
+          <p>Balance: {projectBalance}</p>
+          <ul>
+            {nfts.map((nft, index) => (
+              <li key={index}>
+                <p>NFT!</p>
+                {/* Render NFT metadata here */}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+
+//   return (
+//     <div style={{ textAlign: 'center'}}>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={increment}>Increment</button>
+//     </div>
+//   );
+}
