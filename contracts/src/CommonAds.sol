3186c5833b5d4a198b31c1702d624f051a205e99
sophiahdew <59624772+sophiahdew@users.noreply.github.com>
2023-09-21 14:30:51 +0200

    updated contract

--- contracts/src/CommonAds.sol
+++ contracts/src/CommonAds.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.21;
 
 import {ERC721} from "solady/tokens/ERC721.sol";
-import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+import {FixedPointMathLib as Math} from "solady/utils/FixedPointMathLib.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
 contract CommonAds is ERC721 {
-    using FixedPointMathLib for uint256;
+    using Math for uint256;
     using SafeTransferLib for address;
 
     struct Metadata {
@@ -46,7 +46,7 @@ contract CommonAds is ERC721 {
     error PaymentBelowPrice();
 
     constructor() {
-        // AUCTION_DECAY =
+        AUCTION_DECAY = Math.lnWad(0.1e18) / int(1 days);
     }
 
     function setMetadata(uint256 subId, Metadata calldata meta) external {
@@ -66,7 +66,7 @@ contract CommonAds is ERC721 {
         space.metaId = _getMetaId(msg.sender, subId);
         space.owner = msg.sender;
         space.totalSpots = totalPrices;
-        for (uint256 i = 0; i < totalPrices;) {
+        for (uint256 i = 0; i < totalPrices; ) {
             _mint(msg.sender, _getSpotId(spaceId, i));
             space.spots[i] = Spot({
                 lastUpdatedAt: block.timestamp,
@@ -86,7 +86,11 @@ contract CommonAds is ERC721 {
         _getSpot(spotId).setPrice = newPrice;
     }
 
-    function buy(uint256 spotId, uint256 metaSubId, uint256 newPrice) external payable {
+    function buy(
+        uint256 spotId,
+        uint256 metaSubId,
+        uint256 newPrice
+    ) external payable {
         uint256 price = getPrice(spotId);
         if (msg.value < price) revert PaymentBelowPrice();
         Spot storage spot = _getSpot(spotId);
@@ -100,12 +104,14 @@ contract CommonAds is ERC721 {
         }
     }
 
-    function getSpace(uint256 spaceId) external view returns (address owner, SpotDetails[] memory spots) {
+    function getSpace(
+        uint256 spaceId
+    ) external view returns (address owner, SpotDetails[] memory spots) {
         Space storage space = spaces[spaceId];
         owner = space.owner;
         uint256 totalSpots = space.totalSpots;
         spots = new SpotDetails[](totalSpots);
-        for (uint256 i = 0; i < totalSpots;) {
+        for (uint256 i = 0; i < totalSpots; ) {
             Spot storage spot = space.spots[i];
             spots[i].metadata = metadata[spot.metaId];
             spots[i].price = getPrice(_getSpotId(spaceId, i));
@@ -117,7 +123,10 @@ contract CommonAds is ERC721 {
         }
     }
 
-    function getMetadata(address owner, uint256 subId) external view returns (Metadata memory) {
+    function getMetadata(
+        address owner,
+        uint256 subId
+    ) external view returns (Metadata memory) {
         return metadata[_getMetaId(owner, subId)];
     }
 
@@ -139,12 +148,18 @@ contract CommonAds is ERC721 {
         return "MISSING";
     }
 
-    function _getSpotId(uint256 spaceId, uint256 spotIndex) internal pure returns (uint256 spotId) {
+    function _getSpotId(
+        uint256 spaceId,
+        uint256 spotIndex
+    ) internal pure returns (uint256 spotId) {
         assert(spotIndex < 256);
         spotId = (spaceId << 8) | spotIndex;
     }
 
-    function _getMetaId(address owner, uint256 subId) internal pure returns (bytes32 metaId) {
+    function _getMetaId(
+        address owner,
+        uint256 subId
+    ) internal pure returns (bytes32 metaId) {
         assembly {
             mstore(0x00, subId)
             mstore(0x20, owner)
@@ -152,13 +167,18 @@ contract CommonAds is ERC721 {
         }
     }
 
-    function _getSpot(uint256 spotId) internal view returns (Spot storage spot) {
+    function _getSpot(
+        uint256 spotId
+    ) internal view returns (Spot storage spot) {
         uint256 spaceId = spotId >> 8;
         uint256 spotIndex = spotId & 0xff;
         return spaces[spaceId].spots[spotIndex];
     }
 
-    function _authorized(address operator, uint256 tokenId) internal view returns (bool) {
+    function _authorized(
+        address operator,
+        uint256 tokenId
+    ) internal view returns (bool) {
         return ownerOf(tokenId) == operator;
     }
 }
